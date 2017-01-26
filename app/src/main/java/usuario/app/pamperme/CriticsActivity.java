2fb4da41ed035f615a4709021bbb0325a64cb2aa
herculano96 <herculano.barros@my.istec.pt>
2017-01-26 22:31:46 +0000

    //Feito as classes do Cliente, Criticas , Babysitter e Eventos

--- app/src/main/java/usuario/app/pamperme/CriticsActivity.java
+++ app/src/main/java/usuario/app/pamperme/CriticsActivity.java
@@ -5,5 +5,48 @@ package usuario.app.pamperme;
  */
 
 public class CriticsActivity {
-    
+
+    int idvisit;
+    int idclient;
+    String description;
+    float evaluation;
+
+    public CriticsActivity(float evaluation, int idvisit, int idclient, String description) {
+        this.evaluation = evaluation;
+        this.idvisit = idvisit;
+        this.idclient = idclient;
+        this.description = description;
+    }
+
+    public int getIdvisit() {
+        return idvisit;
+    }
+
+    public void setIdvisit(int idvisit) {
+        this.idvisit = idvisit;
+    }
+
+    public int getIdclient() {
+        return idclient;
+    }
+
+    public void setIdclient(int idclient) {
+        this.idclient = idclient;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(float evaluation) {
+        this.evaluation = evaluation;
+    }
 }
