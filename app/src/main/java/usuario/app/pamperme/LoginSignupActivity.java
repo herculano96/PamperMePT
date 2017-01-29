62465ee5e82cc84eef7b51c59a55371fabff231c
herculano96 <herculano.barros@my.istec.pt>
2017-01-29 17:58:04 +0000

    //Feito as classes do Cliente, Criticas , Babysitter e Eventos

---
+++ app/src/main/java/usuario/app/pamperme/LoginSignupActivity.java
@@ -0,0 +1,9 @@
+package usuario.app.pamperme;
+
+/**
+ * Created by herculanobarros on 29/01/2017.
+ */
+
+public class LoginSignupActivity {
+
+}
--- app/src/main/java/usuario/app/pamperme/NavigationDrawerActivity.java
+++ app/src/main/java/usuario/app/pamperme/NavigationDrawerActivity.java
@@ -2,8 +2,6 @@ package usuario.app.pamperme;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class NavigationDrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,8 +27,7 @@ public class NavigationDrawerActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(NavigationDrawerActivity.this, "Not working yet", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -55,7 +54,7 @@ public class NavigationDrawerActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        getMenuInflater().inflate(R.menu.menu2_app, menu);
         return true;
     }
 
@@ -66,7 +65,7 @@ public class NavigationDrawerActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+
         if (id == R.id.action_settings) {
             return true;
         }
